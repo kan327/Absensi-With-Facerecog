674b90374d88653906abd129b15c5b85c3a46535
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2022-12-08 08:53:16 +0700

    tsaqif trying js

--- resources/views/guru/cam_absen_masuk.blade.php
+++ resources/views/guru/cam_absen_masuk.blade.php
@@ -1,4 +1,7 @@
 @extends('main_guru')
 @section('content')
-    
+    <video id="videoInput" width="720" height="550" muted autoplay loop playsinline>
+
+    <script src="{{ asset('cam_js/js/face-api.min.js') }}"></script>
+    <script src="{{ asset('cam_js/js/script.js') }}"></script>
 @endsection
