372b7b409d10e725f6241ff684d331998fdfa4d7
tsaqifmaulana444 <tsaqifdepok@gmail.com>
2022-12-10 10:08:25 +0700

    tsaqif js

--- resources/views/guru/cam/camdaftar.blade.php
+++ resources/views/guru/cam/camdaftar.blade.php
@@ -81,42 +81,25 @@
     </div>
     {{-- <script src="https://codepen.io/kan327/pen/PoaZWxe"></script> --}}
     {{-- cam js --}}
-    <script src="{{ asset('cam_js/js/webcam.min.js') }}"></script>
-    <script src="{{ asset('assets/JS/noticme.min.js') }}"></script>
-    <script>
-        function configure(){
-            Webcam.set({
-                width: 520,
-                height: 380,
-                image_format: 'jpeg',
-                jpeg_quality: 90,
-                // force_flash: true,
-            })
-
-            Webcam.attach("#mycam")
-        }
-
-        function saveSnap() {
-            Webcam.snap(function (data_uri) {
-                document.getElementById("results").innerHTML = '<img id ="webcam" src = " '+ data_uri +' ">'
-            })
-            
-            Webcam.reset()
-
-            // var csrf = document.getElementsByName("csrf-token")
-            
-            var base64image = document.getElementById("webcam").src
-            Webcam.upload(base64image, '/data_siswa/simpan', function (code, text){
-                // Noticme.any({
-                // text: "Wajah Berhasil Disimpan",
-                // type: "success",
-                // timer: 3000,
-                // })
-                console.log(text)
-                alert(code)
-            })
-        }
-        </script>
+    {{-- <script src="{{ asset('cam_js/js/webcam.min.js') }}"></script>
+    <script src="{{ asset('assets/JS/noticme.min.js') }}"></script> --}}
+    
 </body>
 </html>
       
+
+    {{-- php
+if(isset($_POST['photoStore'])) {
+    $encoded_data = $_POST['photoStore'];
+    $binary_data = base64_decode($encoded_data);
+
+    $photoname = uniqid().'.jpg';
+
+    $result = file_put_contents('uploadPhoto/'.$photoname, $binary_data);
+
+    if($result) {
+        echo 'success';
+    } else {
+        echo die('Could not save image! check file permission.');
+    }
+} --}}
