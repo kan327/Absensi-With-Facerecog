c4c318e469e4a7b5e6b1635a610f3741332189c8
LumiNovryM <luminovrymekel@gmail.com>
2022-12-26 08:14:08 +0700

    Bot-Telegram : Schema Database

---
+++ database/migrations/2022_12_24_140433_bot_admins.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bot_admins', function (Blueprint $table) {
+            $table->integer('chat_id', 10);
+            $table->string('nama');
+            $table->string('email');
+            $table->string('status');
+            $table->string('tanggal');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bot_admins');
+    }
+};
